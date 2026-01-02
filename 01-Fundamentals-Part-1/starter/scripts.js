4cd5803b26f33873a6e96e8aa5ce9c39286fdccd
Keshav-CUJ <ke23becse26@cujammu.ac.in>
2026-01-03 04:44:00 +0530

    Done till data-types

---
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -0,0 +1,27 @@
+console.log("Keshav making changes");
+
+//Variable naming
+// let 3year = 3; //wrong
+let $year = 3;
+let _year = 3;
+let Year = 3; //Not syntax error but we use it for objects.
+let PI = 3.14; //Used for universal constants, dont use let in this case.
+// let year&day = 4; //wrong
+let year3 =3;
+
+console.log(year3);
+
+console.log("Assignment on values and variables");
+
+let country = "India";
+let continent = "Asia";
+let population = 1500;
+
+console.log(country, continent, population);
+
+console.log("Assignment for data-types");
+
+let isIsland = false;
+let language;
+
+console.log(typeof country,typeof continent,typeof population, typeof isIsland, typeof language, typeof null);
