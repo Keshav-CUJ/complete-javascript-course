ef3f144592e0c6d38a8b3911f47ac3c94e7f2731
Keshav-CUJ <ke23becse26@cujammu.ac.in>
2026-01-04 03:56:30 +0530

    Done till operator precedence

---
+++ 01-Fundamentals-Part-1/starter/codingChallenge1.js
@@ -0,0 +1,10 @@
+const massOfMark = 78, massOfJohn= 92, heightOfMark= 1.69, heightOfJohn=1.95;
+
+const markBMI = massOfMark/heightOfMark**2;
+
+const johnBMI = massOfJohn/heightOfJohn**2;
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(johnBMI, markBMI, markHigherBMI);
+
--- 01-Fundamentals-Part-1/starter/scripts.js
+++ 01-Fundamentals-Part-1/starter/scripts.js
@@ -1,6 +1,8 @@
 console.log("Keshav making changes");
 
-//Variable naming
+/*Variable naming
+
+
 // let 3year = 3; //wrong
 let $year = 3;
 let _year = 3;
@@ -9,8 +11,11 @@ let PI = 3.14; //Used for universal constants, dont use let in this case.
 // let year&day = 4; //wrong
 let year3 =3;
 
-console.log(year3);
+console.log(year3);  
 
+*/
+
+/* 
 console.log("Assignment on values and variables");
 
 let country = "India";
@@ -25,3 +30,39 @@ let isIsland = false;
 let language;
 
 console.log(typeof country,typeof continent,typeof population, typeof isIsland, typeof language, typeof null);
+
+*/
+
+
+console.log("Assignment for let, const, var");
+
+//var --never use this
+// const -- prefer to use always
+// let -- when needed too much
+
+let language;
+language = "Hindi"; 
+const country= "India" ;
+const continent ="Asia";
+let population = 1500;
+const isIsland= false;
+
+// isIsland = true; //error
+
+
+
+console.log("Basic operators");
+
+console.log("people in each half", population/2 );
+
+population++;
+console.log(population);
+console.log(population>6);
+console.log(population<33);
+
+const discription = country + " is in "+ continent+", and it's "+ population+ " million people speak "+ language;
+
+console.log(discription);
+
+population = 155;
+console.log(discription);  //const value does not change because it does not evaluated again.
